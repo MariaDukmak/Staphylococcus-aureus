b8c3cb039bd2060389a78fa92601fc88e7805328
marya <maryadukmak@gmail.com>
2020-06-09 13:49:37 +0200

    InputVrager.py Class checkit verbeterd

--- Code/InputVrager.py
+++ Code/InputVrager.py
@@ -81,12 +81,12 @@ class Checkit:
         self.max_ = max_
         self.rang_ = range_
 
-    # https://stackoverflow.com/questions/23294658/asking-the-user-for-input-until-they-give-a-valid-response
 
-    def sanitised_input(prompt, type_=None, min_=None, max_=None, range_=None):
-            if min_ is not None and max_ is not None and max_ < min_:
+    def sanitised_input(prompt, type_=None, min_=None, max_=None):
+        """" https://stackoverflow.com/questions/23294658/asking-the-user-for-input-until-they-give-a-valid-response """
+        if min_ is not None and max_ is not None and max_ < min_:
                 raise ValueError("min_ must be less than or equal to max_.")
-            while True:
+        while True:
                 ui = input(prompt)
                 if type_ is not None:
                     try:
@@ -98,20 +98,6 @@ class Checkit:
                     print("Input must be less than or equal to {0}.".format(max_))
                 elif min_ is not None and ui < min_:
                     print("Input must be greater than or equal to {0}.".format(min_))
-                elif range_ is not None and ui not in range_:
-                    if isinstance(range_, range):
-                        template = "Input must be between {0.start} and {0.stop}."
-                        print(template.format(range_))
-                    else:
-                        template = "Input must be {0}."
-                        if len(range_) == 1:
-                            print(template.format(*range_))
-                        else:
-                            expected = " or ".join((
-                                ", ".join(str(x) for x in range_[:-1]),
-                                str(range_[-1])
-                            ))
-                            print(template.format(expected))
                 else:
                     return ui
 
