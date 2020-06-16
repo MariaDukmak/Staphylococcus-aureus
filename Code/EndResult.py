52dc4b9657acb773691ef8f0328bcf6a186d2594
marya <maryadukmak@gmail.com>
2020-06-16 09:05:57 +0200

    added ph and T

--- Code/EndResult.py
+++ Code/EndResult.py
@@ -10,18 +10,33 @@ class EndResult(JsonChecker):
     def __init__(self, bactName:str, temperature: float, pH: float, endTime: float, typeG: int):
         super().__init__(bactName, temperature, pH, endTime, typeG)
 
-    def logistic(self, bact_name: str, time: float, list=[]):
+    def logistic(self, bact_name: str, time: float, pH:float, temerature:float, list=[],):
         """"c = is de beperkende factor, kan tempratuur, ph of max aantaal cellen zijn
           b= is de groeifactor
           a = is de beginwaarde
           De gebuikte formule komt uit een atrtikel die in de bronnen(1) vermeld staat"""""
-
+        #[min_item, optimum_item, max_item]
         groeisFcator = self.read_json(self, bact_name, "gr")
         beperkendeFactor = self.read_json(self, bact_name, "br")
         beginWaarde = self.read_json(self, bact_name, "bw")
+        temp_waardes = self.read_json(self, bact_name, "temp")
+        pH_waardes = self.read_json(self, bact_name, "ph")
+
+        # tt = ((temp_waardes[1]-temp_waardes[0])*(temerature - temp_waardes[1]) - (temp_waardes[1]- temp_waardes[2])
+        #       *(temp_waardes[1] + temp_waardes[0] - 2*temerature))
+        # tt2 = ((temp_waardes[1] - temp_waardes[0]) * tt)
+        # tt3 = ((temerature - temp_waardes[2])*(temerature - temp_waardes[0])**2 / tt2)
+        # print("tt3", tt3)
+        #
+        # phh = ((pH -pH_waardes[0])*(pH-pH_waardes[2]) - (pH -pH_waardes[1])**2)
+        # phh2 = ((pH -pH_waardes[0])*(pH-pH_waardes[2])/phh)
+        #
+        # print("phh2", phh2)
+        # newgroeiFactpr = groeisFcator[0]*tt3 * phh2
+        # print(newgroeiFactpr)
 
         for t in range(0, int(time) + 1):
-            list.append(beperkendeFactor[0] / (1 + beginWaarde[0] * np.exp(-groeisFcator[0] * t)))
+            list.append(beperkendeFactor/ (1 + beginWaarde[0] * np.exp(-groeisFcator[0] * t)))
         return np.array(list)
 
 
@@ -59,7 +74,7 @@ class EndResult(JsonChecker):
                 list.append(eindRange / (1 + begingValue [0]* np.exp(-groeisFactor[0] * temp)))
 
 
-    def log_growth(self, bact_name, time, lijst=[]):
+    def log_growth(self, bact_name, time,  pH:float, temerature:float, lijst=[]):
 
         """Lag-fase: de periode die verloopt voordat de vermeerdering van de bacteriecellen begint;
           logaritmische fase: de periode, waarin de feitelijke groei plaatsvindt, hierbij verdubbelt het
@@ -75,13 +90,30 @@ class EndResult(JsonChecker):
 
          De cijfers voor bactrie XX komen uit een onderzoek die in de bronnen(3) vermeld staat."""
 
+
+        #https://aem.asm.org/content/aem/61/2/610.full.pdf
         lnN0 = self.read_json(self, bact_name, "bw")
         growthrate = self.read_json(self, bact_name, "gr")
         beperkendeFactor = self.read_json(self, bact_name, "br")
+        temp_waardes = self.read_json(self, bact_name, "temp")
+        pH_waardes = self.read_json(self, bact_name, "ph")
         lijst.append(lnN0[0])
 
+        tt = ((temp_waardes[1]-temp_waardes[0])*(temerature - temp_waardes[1]) - (temp_waardes[1] - temp_waardes[2])
+              * (temp_waardes[1] + temp_waardes[0] - 2*temerature))
+        tt2 = ((temp_waardes[1] - temp_waardes[0]) * tt)
+        tt3 = ((temerature - temp_waardes[2])*(temerature - temp_waardes[0])**2 / tt2)
+        print("tt3", tt3)
+
+        phh = ((pH - pH_waardes[0])*(pH-pH_waardes[2]) - (pH - pH_waardes[1])**2)
+        phh2 = ((pH - pH_waardes[0])*(pH-pH_waardes[2])/phh)
+
+        print("phh2", phh2)
+        newgroeiFactpr = growthrate[0]*tt3 * phh2
+        print(newgroeiFactpr)
+
         for t in range(0, int(time)+1):
-             lnN = (growthrate[0]*(t)) + lnN0[0]
+             lnN = (newgroeiFactpr * t) + lnN0[0]
              if lnN < beperkendeFactor[0]:
                 lijst.append(lnN)
              else:
@@ -101,11 +133,11 @@ class EndResult(JsonChecker):
         ph_check = self.values_check(self, bact_input, ph_input, "ph")
         if (temp_check and ph_check) is not None:
             if typeG == 1:
-                antwoord = self.logistic(self, bact_input, end_time)
+                antwoord = self.logistic(self, bact_input, end_time, ph_input, tem_input)
             if typeG == 2:
                 antwoord = self.temp_logistic(self, bact_input, temp_check)
             if typeG == 3:
-                antwoord = self.log_growth(self, bact_input, end_time)
+                antwoord = self.log_growth(self, bact_input, end_time,ph_input, tem_input)
             print("we got this temp after the value check ", temp_check)
             print("we got this ph after the value check ", ph_check)
             return antwoord
