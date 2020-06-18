2d62aa926cd854444b0e32b054dd8fca068a532c
marya <maryadukmak@gmail.com>
2020-06-18 11:55:13 +0200

    .

--- Code/EndResults.py
+++ Code/EndResults.py
@@ -3,38 +3,37 @@ import numpy as np
 from Code.JsonChecker import JsonChecker
 
 
-class EndResults(JsonChecker):
-    def __init__(self, bact_input, tem_input, ph_input, endTime: float, typeG: int):
-        super().__init__(bact_input, tem_input, ph_input, endTime, typeG)
-        self.bact_input = bact_input
-        self.tem_input = tem_input
+class EndResults:
+    def __init__(self, bact_naam: str, temp_input: float, ph_input:float, end_time: float, type_graph: int):
+        super().__init__(bact_naam, temp_input, ph_input, end_time, type_graph)
+        self.bact_naam = bact_naam
+        self.tem_input = temp_input
         self.ph_input = ph_input
-        self.endTime = endTime
-        self.typeG = typeG
+        self.end_time = end_time
+        self.type_graph = type_graph
         self.__new__()
 
-    def __new__(cls,  bact_input, tem_input, ph_input, endTime: float, typeG: int):
-        temp_check = JsonChecker(bact_input, tem_input, ph_input, "temp", tem_input)
+    def __new__(cls, bact_naam: str, temp_input: float, ph_input:float, end_time: float, type_graph: int):
+        temp_check = JsonChecker(bact_naam, temp_input, ph_input, "temp", temp_input)
         temp_check_terug = temp_check.values_check()
 
-        ph_check = JsonChecker(bact_input, tem_input, ph_input, "ph", ph_input)
+        ph_check = JsonChecker(bact_naam, temp_input, ph_input, "ph", ph_input)
         ph_check_terug = ph_check.values_check()
 
         if (temp_check_terug and ph_check_terug) is not None:
-            if typeG == 1:
-                antwoord = cls.logistic( cls, bact_input, endTime, ph_input, tem_input)
-            if typeG == 2:
-                antwoord = cls.temp_logistic(cls, bact_input, temp_check_terug)
-            if typeG == 3:
-                antwoord = cls.log_growth(cls, bact_input, endTime, ph_input, tem_input)
-            print("we got this temp after the value check ", temp_check_terug)
-            print("we got this ph after the value check ", ph_check_terug)
-            print(antwoord)
+            if type_graph == 1:
+                antwoord = cls.logistic( cls, bact_naam, end_time, ph_input, temp_input)
+            # if type_graph == 2:
+            #     # antwoord = cls.temp_logistic(cls, bact_naam, temp_check_terug)
+            if type_graph == 3:
+                antwoord = cls.log_growth(cls, bact_naam, end_time, ph_input, temp_input)
+            # print("we got this temp after the value check ", temp_check_terug)
+            # print("we got this ph after the value check ", ph_check_terug)
             return antwoord
         else:
             raise ValueError("incorrect type of value was entered")
 
-    def log_growth(self, bact_name, time,  pH:float, temerature:float, lijst=[]):
+    def log_growth(self, bact_name: str, time: float, pH: float, temerature: float, lijst = [], lijstDeath = []) -> np.array:
 
         """Lag-fase: de periode die verloopt voordat de vermeerdering van de bacteriecellen begint;
           logaritmische fase: de periode, waarin de feitelijke groei plaatsvindt, hierbij verdubbelt het
@@ -49,17 +48,15 @@ class EndResults(JsonChecker):
          De cijfers voor bactrie XX komen uit een onderzoek die in de bronnen(3) vermeld staat."""
 
 
-        #https://aem.asm.org/content/aem/61/2/610.full.pdf
-        #lnN0 = self.read_json(self, bact_name, "bw")
-        #groeisFactor = self.read_json(self, bact_name, "gr")
-        #beperkendeFactor = self.read_json(self, bact_name, "br")
         temp_check = JsonChecker(bact_name, temerature, pH, "temp", temerature)
         temp_waardes= temp_check.read_json()
 
         ph_check = JsonChecker(bact_name, temerature, pH, "ph", pH)
         pH_waardes = ph_check.read_json()
+
         groeisFcator = JsonChecker(bact_name, temerature, pH, "gr", None)
         groeisFcator_is = groeisFcator.read_json()
+
         beperkendeFactor = JsonChecker(bact_name, temerature, pH, "br", None)
         beperkendeFactor_is = beperkendeFactor.read_json()
 
@@ -70,43 +67,58 @@ class EndResults(JsonChecker):
         # max growth rate(T, pH) = CTPM(T, pH)= optimum growth rate t(T) p(pH)
 
         # temerature t(T)
+        # de noemer stukje 1
         tt = ((temp_waardes[1]-temp_waardes[0])*(temerature - temp_waardes[1])
               - (temp_waardes[1] - temp_waardes[2])
-              * (temp_waardes[1] + temp_waardes[0] - 2*temerature)) # de noemer stukje 1
+              * (temp_waardes[1] + temp_waardes[0] - 2*temerature))
 
-        tt2 = ((temp_waardes[1] - temp_waardes[0]) * tt) # de noemer stukje 2
-        tt3 = ((temerature - temp_waardes[2])*(temerature - temp_waardes[0])**2 / tt2) # de teller
+        # de noemer stukje 2
+        tt2 = ((temp_waardes[1] - temp_waardes[0]) * tt)
+        # de teller
+        tt3 = ((temerature - temp_waardes[2])*(temerature - temp_waardes[0])**2 / tt2)
 
         # pH p(pH)
-        phh = ((pH - pH_waardes[0])*(pH-pH_waardes[2]) - (pH - pH_waardes[1])**2) # de noemer
-        phh2 = ((pH - pH_waardes[0])*(pH-pH_waardes[2])/phh) # de teller
+        # de noemer
+        phh = ((pH - pH_waardes[0])*(pH-pH_waardes[2]) - (pH - pH_waardes[1])**2)
+        # de teller
+        phh2 = ((pH - pH_waardes[0])*(pH-pH_waardes[2])/phh)
 
         # new groei factor
-        newgroeiFactpr = groeisFcator_is[0] * tt3 * phh2
+        newgroeiFactor = groeisFcator_is[0] * tt3 * phh2
 
         for t in range(0, int(time)+1):
-            lnN = (newgroeiFactpr * t) + lnN0[0]
+            lnN = (newgroeiFactor * t) + (lijst[-1] )
             if lnN < beperkendeFactor_is[0]:
                 lijst.append(lnN)
             else:
                 lijst.append(beperkendeFactor_is[0])
         M = max(lijst) - min(lijst)
 
-        lijst2 = [int(item) for item in lijst[::-1] if int(item) != int(lnN0[0])]
-        unique = [x for i, x in enumerate(lijst2) if i == lijst2.index(x)]
-
-        for item in unique:
+        lijstDeath.append(beperkendeFactor_is[0])
+        while lijstDeath[-1] >= lnN0[0]:
+            antwoord= lijstDeath[-1] - (newgroeiFactor*len(lijstDeath))
+            if antwoord >= lnN0[0]:
+                 lijstDeath.append(antwoord)
+            else:
+                lijstDeath.append(lnN0[0])
+                break
+        print("death",lijstDeath)
+        for item in lijstDeath:
             lijst.append(item)
+        print("lijst", lijst)
         return np.array(lijst)
 
-    def logistic(self, bact_name: str, time: float, pH: float, temerature: float, list=[], ):
+    def logistic(self, bact_name: str, time: float, pH: float, temerature: float, list =[]) -> np.array:
         """"c = is de beperkende factor, kan tempratuur, ph of max aantaal cellen zijn
           b= is de groeifactor
           a = is de beginwaarde
           De gebuikte formule komt uit een atrtikel die in de bronnen(1) vermeld staat"""""
+        temp_check = JsonChecker(bact_name, temerature, pH, "temp", temerature)
+        temp_waardes = temp_check.read_json()
 
+        ph_check = JsonChecker(bact_name, temerature, pH, "ph", pH)
+        pH_waardes = ph_check.read_json()
 
-        #[min_item, optimum_item, max_item]
         groeisFcator = JsonChecker(bact_name, temerature, pH,"gr", None)
         groeisFcator_is = groeisFcator.read_json()
         beperkendeFactor = JsonChecker(bact_name, temerature, pH,"br", None)
@@ -118,42 +130,67 @@ class EndResults(JsonChecker):
         begingValue_is = JsonChecker(bact_name, None, None, "gr", None)
         begingValue = begingValue_is.read_json()
 
+        # max growth rate(T, pH) = CTPM(T, pH)= optimum growth rate t(T) p(pH)
+
+        # temerature t(T)
+        # de noemer stukje 1
+        tt = ((temp_waardes[1] - temp_waardes[0]) * (temerature - temp_waardes[1])
+              - (temp_waardes[1] - temp_waardes[2])
+              * (temp_waardes[1] + temp_waardes[0] - 2 * temerature))
+
+        # de noemer stukje 2
+        tt2 = ((temp_waardes[1] - temp_waardes[0]) * tt)
+        # de teller
+        tt3 = ((temerature - temp_waardes[2]) * (temerature - temp_waardes[0]) ** 2 / tt2)
+
+        # pH p(pH)
+        # de noemer
+        phh = ((pH - pH_waardes[0]) * (pH - pH_waardes[2]) - (pH - pH_waardes[1]) ** 2)
+        # de teller
+        phh2 = ((pH - pH_waardes[0]) * (pH - pH_waardes[2]) / phh)
+
+        # new groei factor
+        newgroeiFactor =groeisFcator_is[0] * tt3 * phh2
+
+
+
         for t in range(0, int(time) + 1):
-            list.append(beperkendeFactor_is[0]/ (1 + begingValue[0] * np.exp(-groeisFcator_is[0] * t)))
+            ant= (beperkendeFactor_is[0]/ (1 + begingValue[0] * np.exp(-newgroeiFactor * t)))
+            if ant <= beperkendeFactor_is[0]:
+                list.append(ant)
         return np.array(list)
 
 
-    def temp_logistic(self, bact_name: str, temp_check:list, list = []):
-        """hier moet de grafiek van de growth getekend, in verglijking met de tempratuur verandering per uur
-             begint bij de min temp en eindigt bij de max
-             De gebruikt formule komt uit een atrikel die in de bronnen(2) vermeld staat """
-
-        beginRange, eindRange = 0, 0
-        groeisFactor_is = JsonChecker(bact_name, None, None ,"gr", None)
-        groeisFactor = groeisFactor_is.read_json()
-
-        if temp_check is not None:
-            if len(temp_check) == 3:
-                beginRange = temp_check[0]
-                eindRange = temp_check[2] + 1
-                # optimum?? moeten iets ermee doen
-            elif len(temp_check) == 2:
-                beginRange = temp_check[0]
-                eindRange = temp_check[1] + 1
-
-            elif len(temp_check) == 1:
-                beginRange = temp_check[0]
-                eindRange = temp_check[0] + 1
-
-        #begingValue = self.read_json(self, bact_name, "bw")
-        begingValue_is = JsonChecker(bact_name, None, None,"bw", None)
-        begingValue = begingValue_is.read_json()
-        for temp in range(int(beginRange), int(eindRange+1)):
-            if list:
-                if list[-1] != eindRange:
-                    list.append(eindRange / (1 + begingValue[0] * np.exp(-groeisFactor[0] * temp)))
-                else:
-                    print(np.array(list))
-                    return np.array(list)
-            else:
-                list.append(eindRange / (1 + begingValue [0]* np.exp(-groeisFactor[0] * temp)))
+    # def temp_logistic(self, bact_name: str, temp_check:list, list = []):
+    #     """hier moet de grafiek van de growth getekend, in verglijking met de tempratuur verandering per uur
+    #          begint bij de min temp en eindigt bij de max
+    #          De gebruikt formule komt uit een atrikel die in de bronnen(2) vermeld staat """
+    #
+    #     beginRange, eindRange = 0, 0
+    #     groeisFactor_is = JsonChecker(bact_name, None, None ,"gr", None)
+    #     groeisFactor = groeisFactor_is.read_json()
+    #
+    #     if temp_check is not None:
+    #         if len(temp_check) == 3:
+    #             beginRange = temp_check[0]
+    #             eindRange = temp_check[2] + 1
+    #             # optimum?? moeten iets ermee doen
+    #         elif len(temp_check) == 2:
+    #             beginRange = temp_check[0]
+    #             eindRange = temp_check[1] + 1
+    #
+    #         elif len(temp_check) == 1:
+    #             beginRange = temp_check[0]
+    #             eindRange = temp_check[0] + 1
+    #
+    #     begingValue_is = JsonChecker(bact_name, None, None,"bw", None)
+    #     begingValue = begingValue_is.read_json()
+    #     for temp in range(int(beginRange), int(eindRange+1)):
+    #         if list:
+    #             if list[-1] != eindRange:
+    #                 list.append(eindRange / (1 + begingValue[0] * np.exp(-groeisFactor[0] * temp)))
+    #             else:
+    #                 print(np.array(list))
+    #                 return np.array(list)
+    #         else:
+    #             list.append(eindRange / (1 + begingValue [0]* np.exp(-groeisFactor[0] * temp)))
